237a6bef0e875a6c0a23afa8927ba1dd0b30ea1c
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-05-27 21:18:39 -0700

    test dir

--- api/index.php
+++ api/index.php
@@ -1,14 +1,24 @@
 <?php
 $dir    = __DIR__;
 $files1 = scandir($dir);
+
+echo '<pre>';
+print_r(getcwd());
 print_r($files1);
 
-print_r(getcwd());
 
 $dir = dirname(getcwd());
 $files1 = scandir($dir);
+print_r($dir);
 print_r($files1);
 
-print_r($dir);
+
+
+$dir    = __DIR__.'/../public/';
+$files1 = scandir($dir);
+
+print_r($files1);
+
+return;
 
 require __DIR__ . '/../public/index.php';
