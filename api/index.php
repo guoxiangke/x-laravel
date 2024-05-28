b9d60f739ac1d69d7737a64e0db6c8075b066ec1
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-05-27 21:25:17 -0700

    test dir

--- api/index.php
+++ api/index.php
@@ -1,22 +1,12 @@
 <?php
-$dir    = __DIR__;
-$files1 = scandir($dir);
-
+// 从请求 URI 中解析出路径部分
+$path = parse_url($requestUri, PHP_URL_PATH);
 echo '<pre>';
-print_r(getcwd());
-print_r($files1);
+echo $path;
 
 
-$dir = dirname(getcwd());
+$dir    = __DIR__.'/../public/build/assets/';
 $files1 = scandir($dir);
-print_r($dir);
-print_r($files1);
-
-
-
-$dir    = __DIR__.'/../public/';
-$files1 = scandir($dir);
-
 print_r($files1);
 
 return;
