e91119591ea09e258220633954e607fae82ee72a
XIANGKE GUO <mbp@XIANGKEs-MacBook-Pro.local>
2024-05-27 21:14:34 -0700

    test dir

--- api/index.php
+++ api/index.php
@@ -1,2 +1,14 @@
 <?php
+$dir    = __DIR__;
+$files1 = scandir($dir);
+print_r($files1);
+
+print_r(getcwd());
+
+$dir = dirname(getcwd());
+$files1 = scandir($dir);
+print_r($files1);
+
+print_r($dir);
+
 require __DIR__ . '/../public/index.php';
